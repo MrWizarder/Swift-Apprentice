f3d25150d7abaf41d88c3f057edb96ae1e3dd763
wizarder <631031385@qq.com>
2018-05-30 23:18:33 +0800

    15.introduction

--- 15.Enumerations.playground/Contents.swift
+++ 15.Enumerations.playground/Contents.swift
@@ -4,3 +4,77 @@ import UIKit
 
 
 //// Your first enumeration
+
+let months = ["January", "February", "March", "April", "May",
+              "June", "July", "August", "September", "October",
+              "November", "December"]
+
+//func semester(for month: String) -> String {
+//    switch month {
+//    case "August", "September", "October", "November", "December":
+//        return "Autumn"
+//    case "January", "February", "March", "April", "May":
+//        return "Spring"
+//    default:
+//        return "Not in the school year"
+//    }
+//}
+//
+//semester(for: "April") // Spring
+
+
+//// Declaring an enumeration
+
+//enum Month {
+//    case january
+//    case february
+//    case march
+//    case april
+//    case may
+//    case june
+//    case july
+//    case august
+//    case september
+//    case october
+//    case november
+//    case december
+//}
+
+enum Month {
+    case january, february, march, april, may, june, july, august, september, october, november, decimber
+}
+
+
+//// Deciphering an enumeration in a function
+
+//func semester(for month: Month) -> String {
+//    switch month {
+//    case Month.august, Month.september, Month.october, Month.november, Month.decimber:
+//        return "Autumn"
+//    case Month.january, Month.february, Month.march, Month.april, Month.may:
+//        return "Spring"
+//    default:
+//        return "Not in the school year"
+//    }
+//}
+
+func semester(for month: Month) -> String {
+    switch month {
+    case .august, .september, .october, .november, .decimber:
+        return "Autumn"
+    case .january, .february, .march, .april, .may:
+        return "Spring"
+    case .june, .july:
+        return "Summer"
+    }
+}
+
+var month = Month.april
+semester(for: month)
+
+month = .september
+semester(for: month)
+
+
+//// Code completion prevents typos
+
