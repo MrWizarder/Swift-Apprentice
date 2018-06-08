366b87265c8b54f1e7d507ea42b96a0e211a7e79
wizarder <631031385@qq.com>
2018-06-08 09:02:26 +0800

    19.custom_operators

---
+++ 19.Custom_Operators,Subscripts_and_Keypaths.playground/Contents.swift
@@ -0,0 +1,80 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//// Custom operators
+
+infix operator **
+
+func **(base: Int, power: Int) -> Int {
+    precondition(power >= 2)
+    var result = base
+    for _ in 2...power {
+        result *= base
+    }
+    return result
+}
+
+let base = 2
+let exponent = 2
+let result = base ** exponent
+
+
+//// Compound assignment operator
+
+
+infix operator **=
+
+func **=(lhs: inout Int, rhs: Int) {
+    lhs = lhs ** rhs
+}
+
+var number = 2
+number **= exponent
+
+
+//// Generic operators
+
+func **<T: BinaryInteger>(base: T, power: Int) -> T {
+    precondition(power >= 2)
+    var result = base
+    for _ in 2...power {
+        result *= base
+    }
+    return result
+}
+
+func **=<T: BinaryInteger>(lhs: inout T, rhs: Int) {
+    lhs = lhs ** rhs
+}
+
+
+let unsignedBase: UInt = 2
+let unsignedResult = unsignedBase ** exponent
+
+let base8: Int8 = 2
+let result8 = base8 ** exponent
+
+let unsignedBase8: UInt8 = 2
+let unsignedResult8 = unsignedBase8 ** exponent
+
+let base16: Int16 = 2
+let result16 = base16 ** exponent
+
+let unsignedBase16: UInt16 = 2
+let unsignedResult16 = unsignedBase16 ** exponent
+
+let base32: Int32 = 2
+let result32 = base32 ** exponent
+
+let unsignedBase32: UInt32 = 2
+let unsignedResult32 = unsignedBase32 ** exponent
+
+let base64: Int64 = 2
+let result64 = base64 ** exponent
+
+let unsignedBase64: UInt64 = 2
+let unsignedResult64 = unsignedBase64 ** exponent
+
+
+
