50455405c680285e0f46e3d120fd58144668ba59
wizarder <631031385@qq.com>
2018-06-12 08:34:53 +0800

    19.chanllenge

--- 19.Custom_Operators,Subscripts_and_Keypaths.playground/Contents.swift
+++ 19.Custom_Operators,Subscripts_and_Keypaths.playground/Contents.swift
@@ -231,4 +231,27 @@ jukebox[keyPath: song] = "Stairway to heaven"
 //// Challenges
 
 // 1
+extension Array {
+    subscript(index: Int) -> (String, String)? {
+        get {
+        guard let value = self[index] as? Int else {
+            return nil
+        }
+        
+        switch (value >= 0, abs(value)%2) {
+        case (true, 0):
+            return ("positive", "even")
+        case (true: 1):
+            return ("positive", "odd")
+        case (false, 0):
+            return ("negative", "even")
+        case (false, 1):
+            return ("negative", "odd")
+        default:
+            return nil
+        }
+        }
+    }
+}
+
 
