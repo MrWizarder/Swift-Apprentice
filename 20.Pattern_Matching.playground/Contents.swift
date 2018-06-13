a70a6dad76cf676f1a88c18658d0d5e933e24028
wizarder <631031385@qq.com>
2018-06-13 08:40:44 +0800

    20.switch

--- 20.Pattern_Matching.playground/Contents.swift
+++ 20.Pattern_Matching.playground/Contents.swift
@@ -27,18 +27,39 @@ if case (_, 0, 0) = coordinate {
 //    return "Not at origin"
 //}
 
-func process(point: (x: Int, y: Int, z: Int)) -> String {
-    guard case(0, 0, 0) = point else {
-        return "Not at origin"
-    }
-    return "At origin"
-}
+//func process(point: (x: Int, y: Int, z: Int)) -> String {
+//    guard case(0, 0, 0) = point else {
+//        return "Not at origin"
+//    }
+//    return "At origin"
+//}
 
-let point = (x: 0, y: 0, z: 0)
-let response = process(point: point)
+//let point = (x: 0, y: 0, z: 0)
+//let response = process(point: point)
 
 
 
 //// switch
+
+func process(point: (x: Int, y: Int, z: Int)) -> String {
+    // 1
+    let closeRange = -2...2
+    let midRange = -5...5
+    // 2
+    switch point {
+    case (0, 0, 0):
+        return "At origin"
+    case (closeRange, closeRange, closeRange):
+        return "Very close to origin"
+    case (midRange, midRange, midRange):
+        return "Nearby origin"
+    default:
+        return "Not near origin"
+    }
+}
+
+let point = (x: 15, y: 5, z: 3)
+let response = process(point: point)
+
 
 
